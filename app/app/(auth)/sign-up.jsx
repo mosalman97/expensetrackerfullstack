50c34c3f374cac875dc52b0f574fad15e1ae118d
mosalman97 <mosalman1098@gmail.com>
2026-01-02 07:08:50 +0400

    signup screen ui wip

---
+++ app/app/(auth)/sign-up.jsx
@@ -0,0 +1,182 @@
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "../../assets/styles/auth.styles.js";
+import { COLORS } from "@/constants/colors.js";
+
+export default function SignUpScreen() {
+	const { isLoaded, signUp, setActive } = useSignUp();
+	const router = useRouter();
+
+	const [emailAddress, setEmailAddress] = useState("");
+	const [password, setPassword] = useState("");
+	const [pendingVerification, setPendingVerification] = useState(false);
+	const [code, setCode] = useState("");
+	const [error, setError] = useState("");
+
+	// Handle submission of sign-up form
+	const onSignUpPress = async () => {
+		if (!isLoaded) return;
+
+		// Start sign-up process using email and password provided
+		try {
+			await signUp.create({
+				emailAddress,
+				password,
+			});
+
+			// Send user an email with verification code
+			await signUp.prepareEmailAddressVerification({
+				strategy: "email_code",
+			});
+
+			// Set 'pendingVerification' to true to display second form
+			// and capture OTP code
+			setPendingVerification(true);
+		} catch (err) {
+			// See https://clerk.com/docs/custom-flows/error-handling
+			// for more info on error handling
+			console.error(JSON.stringify(err, null, 2));
+		}
+	};
+
+	// Handle submission of verification form
+	const onVerifyPress = async () => {
+		if (!isLoaded) return;
+
+		try {
+			// Use the code the user provided to attempt verification
+			const signUpAttempt = await signUp.attemptEmailAddressVerification({
+				code,
+			});
+
+			// If verification was completed, set the session to active
+			// and redirect the user
+			if (signUpAttempt.status === "complete") {
+				await setActive({ session: signUpAttempt.createdSessionId });
+				router.replace("/");
+			} else {
+				// If the status is not complete, check why. User may need to
+				// complete further steps.
+				console.error(JSON.stringify(signUpAttempt, null, 2));
+			}
+		} catch (err) {
+			// See https://clerk.com/docs/custom-flows/error-handling
+			// for more info on error handling
+			console.error(JSON.stringify(err, null, 2));
+		}
+	};
+
+	if (pendingVerification) {
+		return (
+			<View style={styles.verificationContainer}>
+				<Text style={styles.verificationTitle}>Verify your email</Text>
+				{error ? (
+					<View style={styles.errorBox}>
+						<Ionicons
+							name="alert-circle"
+							size={20}
+							color={COLORS.expense}
+						/>
+						<Text style={styles.errorText}>{error}</Text>
+						<TouchableOpacity onPress={() => setError("")}>
+							<Ionicons
+								name="close"
+								size={20}
+								color={COLORS.textLight}
+							/>
+						</TouchableOpacity>
+					</View>
+				) : null}
+
+				<TextInput
+					value={code}
+					placeholder="Enter your verification code"
+					onChangeText={(code) => setCode(code)}
+					style={[
+						styles.verificationInput,
+						error && styles.errorInput,
+					]}
+					placeholderTextColor="#9A8478"
+					onChange={(text) => {
+						setCode(text);
+					}}
+				/>
+				<TouchableOpacity onPress={onVerifyPress} style={styles.button}>
+					<Text style={styles.buttonText}>Verify</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<View style={styles.container}>
+				<Image
+					style={styles.illustration}
+					source={require("../../assets/images/revenue-i2.png")}
+				/>
+				<Text style={styles.title}>Create Account</Text>
+				{error ? (
+					<View style={styles.errorBox}>
+						<Ionicons
+							name="alert-circle"
+							size={20}
+							color={COLORS.expense}
+						/>
+						<Text style={styles.errorText}>{error}</Text>
+						<TouchableOpacity onPress={() => setError("")}>
+							<Ionicons
+								name="close"
+								size={20}
+								color={COLORS.textLight}
+							/>
+						</TouchableOpacity>
+					</View>
+				) : null}
+				<TextInput
+					autoCapitalize="none"
+					value={emailAddress}
+					placeholder="Enter your Email"
+					onChangeText={(email) => setEmailAddress(email)}
+					style={[styles.input, error && styles.errorInput]}
+					placeholderTextColor="#9A8478"
+				/>
+				<TextInput
+					value={password}
+					placeholder="Enter your Password"
+					onChangeText={(password) => setPassword(password)}
+					style={[styles.input, error && styles.errorInput]}
+					placeholderTextColor="#9A8478"
+					secureTextEntry={true}
+				/>
+				<TouchableOpacity onPress={onSignUpPress} style={styles.button}>
+					<Text style={styles.buttonText}>Sign Up</Text>
+				</TouchableOpacity>
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "center",
+					}}
+				>
+					<Text style={styles.footerText}>
+						Already have an account ?{" "}
+					</Text>
+					<Link href="/sign-in">
+						<Text style={styles.linkText}>Sign in</Text>
+					</Link>
+				</View>
+			</View>
+		</View>
+	);
+}
--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,15 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import SafeScreen from "@/components/SafeScreen";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
 	return (
-		<SafeScreen>
-			<Stack screenOptions={{ headerShown: false }} />;
-		</SafeScreen>
+		<ClerkProvider tokenCache={tokenCache}>
+			<SafeScreen>
+				<Slot />
+			</SafeScreen>
+		</ClerkProvider>
 	);
 }
